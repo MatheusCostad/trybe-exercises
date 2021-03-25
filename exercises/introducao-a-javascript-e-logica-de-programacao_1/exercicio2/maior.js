411d3a57db03005136fd748ef7956ebe15f13443
Matheus Costa <mathws.costag@gmail.com>
2021-03-25 19:00:18 -0300

    exercicio3 concluido

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio2/maior.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio2/maior.js
@@ -2,9 +2,15 @@ let valor1 = 20;
 let valor2 = 30;
 
 if (valor1 > valor2) {
+ 
     console.log(valor1);
+
 } else if (valor2 > valor1) {
+
     console.log(valor2);
+
 } else {
+
     console.log("os valores são iguais");
+
 }

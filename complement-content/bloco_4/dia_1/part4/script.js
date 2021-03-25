646d13eea89e667262bf1fe2288115f387cbd083
Matheus Costa <mathws.costag@gmail.com>
2021-03-25 10:21:13 -0300

    conclue a quarta parte do dia 1 do bloco 4

---
+++ complement-content/bloco_4/dia_1/part4/script.js
@@ -0,0 +1,11 @@
+let nota = 100
+
+if (nota >= 80){
+    console.log("Parabéns, você foi aprovado(o)!");
+} else if (80> nota >=60) {
+    console.log("Você está na nossa lista de espera");
+} else if (nota < 60) {
+    console.log("Você foi reprovada(o)");
+} else {
+    console.log("Nota inválida");
+}

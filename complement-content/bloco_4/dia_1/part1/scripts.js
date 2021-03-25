10ff909d38acbe9bdd855ca0b567f9d92602b995
Matheus Costa <mathws.costag@gmail.com>
2021-03-24 22:16:50 -0300

    conclue a primeria parte do dia 1 do bloco 4

---
+++ complement-content/bloco_4/dia_1/part1/scripts.js
@@ -0,0 +1,10 @@
+const name = 'Matheus Costa';
+const birthCity = 'João Pessoa';
+let birthYear = 2003;
+
+birthCity = 'São Paulo';
+
+console.log(birthCity);
+
+// Mensagem de erro pois const não se altera.
+

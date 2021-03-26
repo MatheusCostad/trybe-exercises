c0e8ee4945dec7547b9e34d20606768e2c14d014
Matheus Costa <mathws.costag@gmail.com>
2021-03-26 10:02:03 -0300

    exercicio11 concluido

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio11/salarioliquido.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio11/salarioliquido.js
@@ -1,17 +1,27 @@
-let salario = 1000.00;
+let salario = 3500.00;
 let salarioINSS = 0;
-let salariofinal = 0;
+let salarioli = 0;
 
 if ( salario < 1556.94 | salario > 1) {
   salarioINSS = salario - salario*(8/100);
 } else if (salario >= 1556.94 | salario <= 2594.92) {
-  salarioINSS = salario - salario*(9/100);
+  salarioINSS =  salario - salario*(9/100);
 } else if (salario >= 2594.93 | salario <= 5189.82) {
   salarioINSS = salario - salario*(11/100);
 } else {
   salarioINSS = salario - 570.88;
 }
 
-if (salario >= 1908.99 | salario <= 2826.65) {
-  salarioIR
+if (salarioINSS >= 1908.99 | salario <= 2826.65) {
+  salarioli = salarioINSS - salarioINSS*(7.5/100) + 142.80;
+} else if (salarioINSS >= 2826.66 | salarioINSS <= 3751.05) {
+  salarioli = salarioINSS - salarioINSS*(15/100) + 354.80;
+} else if (salarioINSS >= 3751.06 | salarioINSS <= 4664.68) {
+  salarioli = salarioINSS - salarioINSS*(22.5/100) + 636.13;
+} else if (salarioINSS >= 4664.69) {
+  salarioli = salarioINSS - salarioINSS*(27.5/100) + 869.36;
+} else {
+  salarioli = salarioINSS - 0;
 }
+
+console.log(salarioli)

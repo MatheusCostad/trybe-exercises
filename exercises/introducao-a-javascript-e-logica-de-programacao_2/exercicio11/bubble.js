96fb4fab536d3833b778e654a31396309009bec5
Matheus Costa <mathws.costag@gmail.com>
2021-03-27 22:48:47 -0300

    exercicio12 concluido

--- exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio11/bubble.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_2/exercicio11/bubble.js
@@ -1,7 +1,5 @@
 let number = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-
-
 for (let index = 1; index < number.length; index += 1) {
   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
     if (number[index] < number[secondIndex]) {

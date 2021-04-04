af97bfbc46bf1f752283e115c989ba905eaca82d
Matheus Costa <mathws.costag@gmail.com>
2021-04-04 18:21:54 -0300

    exercicio5

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/exercicio_5/patinhas.js
@@ -0,0 +1,20 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim',
+};
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'Pato Donald e O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (const key in info) {
+  if (info[key] === info2.recorrente) {
+    console.log('Ambos recorrentes');
+  } else {
+  console.log(`${info[key]} e ${info2[key]}`);}
+}

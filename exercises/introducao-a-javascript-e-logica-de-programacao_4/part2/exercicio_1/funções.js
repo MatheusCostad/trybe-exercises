53c8c3b7b2a6e758ddbc9fde292fe15fdb3ea257
Matheus Costa <mathws.costag@gmail.com>
2021-04-04 19:17:07 -0300

    Cria uma funçao que verifica o index do maior numero em uma array.

--- exercises/introducao-a-javascript-e-logica-de-programacao_4/part2/exercicio_1/funções.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_4/part2/exercicio_1/funções.js
@@ -8,3 +8,25 @@ function verificaPalindrome(word) {
 
 console.log(verificaPalindrome('arara'));
 
+function higherNumber(numbers) {
+  let maior = numbers[0];
+  for (let compare of numbers) {
+    if (compare > maior) {
+      maior = compare
+    } else {
+      maior = maior
+    }
+  }
+  return maior
+}
+
+function higherNumberIndex(numbers) {
+  let maior = higherNumber(numbers);
+for (const key in numbers) {
+  if (maior === numbers[key]) {
+    return key
+  }
+}
+}
+
+console.log(higherNumberIndex([2, 3, 6, 7, 10, 1]));

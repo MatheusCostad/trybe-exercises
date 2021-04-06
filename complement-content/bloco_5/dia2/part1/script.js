fe8c8dea23bd96a73a6f24487eaa50b4632d92bd
Matheus Costa <mathws.costag@gmail.com>
2021-04-06 18:57:32 -0300

    ....

---
+++ complement-content/bloco_5/dia2/part1/script.js
@@ -0,0 +1,15 @@
+let elementoOndeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
+
+console.log(elementoOndeVoceEsta);
+
+let parentNode = document.querySelector('#elementoOndeVoceEsta').parentNode;
+
+console.log(parentNode);
+
+let firstElementChild = document.querySelector('#pai').firstElementChild;
+
+firstElementChild
+ 
+
+console.log(firstElementChild);
+

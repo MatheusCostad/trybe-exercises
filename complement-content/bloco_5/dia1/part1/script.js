69952515b263e6e4a7127b849d13b86ae1e63640
Matheus Costa <mathws.costag@gmail.com>
2021-04-07 11:39:34 -0300

    part1

--- complement-content/bloco_5/dia1/part1/script.js
+++ complement-content/bloco_5/dia1/part1/script.js
@@ -1,20 +1,20 @@
-var paragraph = document.getElementById("first-paragraph");
-paragraph.style.color = "blue";
+var paragraph = document.querySelector('.paragraph');
+paragraph.style.color = "red";
 
 // Recupere o elemento que contém o título da página e faça algo com ele, como alterá-lo para o nome do seu filme favorito.
 // Agora recupere o segundo parágrafo e use sua criatividade para alterá-lo.
 // Por fim, recupere o subtítulo e altere-o também.
 
-document.getElementById('page-title').innerText = 'Perfect Blue';
+document.querySelector('#page-title').innerText = 'Perfect Blue';
 
-let secundParagraph = document.getElementById("second-paragraph");
+let secundParagraph = document.querySelector("#second-paragraph");
 secundParagraph.style.color = "blue";
 
-document.getElementById("subtitle").innerText = 'Akira';
+document.querySelector("#subtitle").innerText = 'Akira';
 
-console.log(document.getElementsByClassName('paragraph'));
+console.log(document.querySelectorAll('.paragraph'));
 
-let firstParagraph = document.getElementsByClassName('paragraph')[0];
+let firstParagraph = document.querySelector('.paragraph');
 firstParagraph.style.width = '20px';
 
-console.log(document.getElementsByTagName('h4'));
+console.log(document.querySelectorAll('h4'));

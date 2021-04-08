ee4d244600d0711b3158e2c94841c3a5abcf3c8c
Matheus Costa <mathws.costag@gmail.com>
2021-04-08 12:02:31 -0300

    complement

--- complement-content/bloco_5/dia2/part1/script.js
+++ complement-content/bloco_5/dia2/part1/script.js
@@ -4,12 +4,18 @@ console.log(elementoOndeVoceEsta);
 
 let parentNode = document.querySelector('#elementoOndeVoceEsta').parentNode;
 
-console.log(parentNode);
+parentNode.style.color = 'blue';
 
-let firstElementChild = document.querySelector('#pai').firstElementChild;
+document.querySelector('#elementoOndeVoceEsta').firstElementChild.innerText = 'Batata.';
 
-firstElementChild
- 
+let firstChild = document.querySelector('#pai').firstElementChild;
 
-console.log(firstElementChild);
+console.log(firstChild);
 
+console.log(document.querySelector('#elementoOndeVoceEsta').previousElementSibling);
+
+console.log(document.querySelector('#elementoOndeVoceEsta').nextSibling);
+
+console.log(document.querySelector('#elementoOndeVoceEsta').nextElementSibling);
+
+console.log(document.querySelector('#pai').lastElementChild.previousElementSibling);

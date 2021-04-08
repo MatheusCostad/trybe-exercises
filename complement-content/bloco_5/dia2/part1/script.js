a85fe6bbd4a37e04656035a8aafca8a3b46ac310
Matheus Costa <mathws.costag@gmail.com>
2021-04-08 13:06:29 -0300

    complement part2

--- complement-content/bloco_5/dia2/part1/script.js
+++ complement-content/bloco_5/dia2/part1/script.js
@@ -19,3 +19,25 @@ console.log(document.querySelector('#elementoOndeVoceEsta').nextSibling);
 console.log(document.querySelector('#elementoOndeVoceEsta').nextElementSibling);
 
 console.log(document.querySelector('#pai').lastElementChild.previousElementSibling);
+
+let pai = document.querySelector('#pai');
+let ondeEstouIrmao = document.createElement('div');
+ondeEstouIrmao.innerText = 'teste';
+pai.appendChild(ondeEstouIrmao);
+
+let ondeEstou = document.querySelector('#elementoOndeVoceEsta');
+let ondeEstouFilho = document.createElement('div');
+ondeEstouFilho.innerText = 'teste';
+ondeEstou.appendChild(ondeEstouFilho);
+
+let primeiroFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho');
+let primeiroFilhoDoFilhoFilho = document.createElement('div');
+primeiroFilhoDoFilhoFilho.innerText = 'test';
+primeiroFilhoDoFilho.appendChild(primeiroFilhoDoFilhoFilho);
+
+console.log(document.querySelector('#primeiroFilhoDoFilho div').parentElement.parentElement.nextElementSibling);
+
+
+
+
+

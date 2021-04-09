a2ba834577ab56573ecfb81009f8e32ffa39de4f
Matheus Costa <mathws.costag@gmail.com>
2021-04-09 00:07:48 -0300

    questao 5 resolvida

--- exercises/javascript-dom-eventos-e-web-storage_2/script.js
+++ exercises/javascript-dom-eventos-e-web-storage_2/script.js
@@ -1,4 +1,3 @@
-// Adicione a tag div com a classe right-content como filho da tag div criada no passo 2;
 // Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do div criado no passo 5;
 // Adicione uma lista não ordenada com os valores de 1 a 10 por extenso como valores da lista. Essa lista deve ser filha do div criado no passo 6;
 // Adicione 3 tags h3 , todas sendo filhas do div criado no passo 2.
@@ -29,3 +28,6 @@ div2.appendChild(paraChildDiv2);
 // Adicione a tag div com a classe left-content como filho da tag div criada no passo 2;
 let div1Child2 = document.createElement('div');
 div1Child2.className = 'left-content';
+div1.appendChild(div1Child2);
+
+// Adicione a tag div com a classe right-content como filho da tag div criada no passo 2;

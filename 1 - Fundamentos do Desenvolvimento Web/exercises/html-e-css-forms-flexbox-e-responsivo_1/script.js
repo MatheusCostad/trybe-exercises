27fc927e5420a66430d93624fb946c387890872c
Matheus Costa <mathws.costag@gmail.com>
2021-04-20 14:01:22 -0300

    exercicio concluido

--- 1 - Fundamentos do Desenvolvimento Web/exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
+++ 1 - Fundamentos do Desenvolvimento Web/exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -1,4 +1,6 @@
 let comboBox = document.getElementById('estados');
+let submitButton = document.getElementById('submitButton');
+let clearButton = document.getElementById('clearButton'); 
 
 function createStates() {
   let comboBox = document.getElementById('estados');
@@ -39,5 +41,64 @@ function createStates() {
     comboBox.appendChild(option);
   }
 }
+createStates();
 
-createStates()
+function validateData(data) {
+  if (data.indexOf('/') === 2 || data.indexOf('/') === 5) {
+    const day = data.substr(0, 2);
+    const month = data.substr(3, 2);
+    const year = data.substr(6, 4);
+    if ((day > 0 && day <= 31) && (month > 0 && month <= 12) && (year >= 0 && year.length === 4)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkData() {
+  const inputData = document.querySelector('#dateText');
+  let data = inputData.value;
+  const userData = validateData(data);
+  if (!userData && data.length) {
+    inputData.value = '';
+    alert('data invalida');
+    return false;
+  }
+  return userData;
+}
+
+function renderCurriculum(event) {
+  event.preventDefault();
+  const formElements = document.querySelectorAll('input');
+  for (let index = 0; index < formElements.length; index += 1) {
+    if (formElements[index].type === 'radio' && !formElements[index].checked) {
+      continue;
+    }
+    const userInput = formElements[index].value;
+    const dataUser = document.querySelector('.form-data');
+    if (checkEmail() && checkData()) {
+      const div = document.createElement('div');
+      div.className = 'div-curriculum';
+      div.innerHTML = userInput;
+      dataUser.appendChild(div);
+    }
+  }
+}
+
+function clearValuer() {
+  const formElements = document.querySelectorAll('input');
+  const textArea = document.querySelector('textarea');
+  for (let index = 0; index < formElements.length && index < div.length; index += 1) {
+    const userInput = formElements[index];
+    userInput.value = '';
+    textArea.value = '';
+  }
+}
+
+submitButton.addEventListener('click', renderCurriculum());
+
+clearButton.addEventListener('click', clearValuer());
+
+window.onload = function () {
+  createStates();
+}

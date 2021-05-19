d67939d8caa1be354302448958c835d24ae26731
Matheus Costa <mathws.costag@gmail.com>
2021-05-19 12:35:26 -0300

    Implementa a função hydrate.

--- 1 - Fundamentos do Desenvolvimento Web/exercises/testes-automatizados-com-jest_1/5 - Identical/identical.test.js
+++ 1 - Fundamentos do Desenvolvimento Web/exercises/testes-automatizados-com-jest_1/5 - Identical/identical.test.js
@@ -1,4 +1,17 @@
-const {obj1, obj2, obj3} = require('./identical');
+const obj1 = {
+  title: 'My Different Title',
+  description: 'My Description',
+};
+
+const obj2 = {
+  title: 'My Different Title',
+  description: 'My Description',
+};
+
+const obj3 = {
+  title: 'My Different Title',
+  description: 'My Description',
+};
 
 test('Compare dois objetos (JSON) para verificar se são idênticos ou não', () => {
   expect(obj1).toEqual(obj2);

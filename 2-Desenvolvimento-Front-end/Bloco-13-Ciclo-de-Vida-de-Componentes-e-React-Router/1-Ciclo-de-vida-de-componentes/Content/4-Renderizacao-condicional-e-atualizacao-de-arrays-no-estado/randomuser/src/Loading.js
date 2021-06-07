54d4d95e50d7ad1ca9047927c116a0b615ad407c
Matheus Costa <mathws.costag@gmail.com>
2021-06-07 13:51:54 -0300

    randomuser

---
+++ 2-Desenvolvimento-Front-end/Bloco-13-Ciclo-de-Vida-de-Componentes-e-React-Router/1-Ciclo-de-vida-de-componentes/Content/4-Renderizacao-condicional-e-atualizacao-de-arrays-no-estado/randomuser/src/Loading.js
@@ -0,0 +1,12 @@
+// Loading
+import React, { Component } from 'react';
+
+class Loading extends Component {
+  render() {
+    return (
+      <div>Carregando...</div>
+    );
+  }
+}
+
+export default Loading;

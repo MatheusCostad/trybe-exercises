c2c14ceb99be3177029ba1f8b3e60c76b214d270
Matheus Costa <mathws.costag@gmail.com>
2021-06-07 20:48:58 -0300

    Componente Switch

---
+++ 2-Desenvolvimento-Front-end/Bloco-13-Ciclo-de-Vida-de-Componentes-e-React-Router/2-React-Router/Content/4-Componentes-BrowserRouter-e-Route/app.js
@@ -0,0 +1,7 @@
+<Route path="/about" >
+<About />
+</Route>
+
+/* <Route path="/about" component={About} />
+<Route path="/contact" component={Contact} />
+<Route exact path="/" component={Home} /> */
---
+++ 2-Desenvolvimento-Front-end/Bloco-13-Ciclo-de-Vida-de-Componentes-e-React-Router/2-React-Router/Content/8-Componente-Switch/switch.js
@@ -0,0 +1,6 @@
+<Switch>
+  <Route path="/about" component={About} />
+  <Route path="/contact" component={Contact} />
+  <Route path="/movies" component={Movies} />
+  <Route path="/" component={Home} />
+</Switch>

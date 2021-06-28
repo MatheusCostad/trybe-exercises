8c40ca8898081c9313d3f95cc8d00036b2fbc71d
Matheus Costa <mathws.costag@gmail.com>
2021-06-28 12:41:37 -0300

    3-Testing-only-one-component

---
+++ 2-Desenvolvimento-Front-end/Bloco-15-Testes-automatizados-com-React-Testing-Library/1-RTL-first-steps/Content/3-Testing-only-one-component/ValidEmail.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ValidEmail from './ValidEmail';
+
+test('Testando um componente, caso o email seja valido.', () => {
+  const EMAIL_USER = 'email@email.com';
+  const { getByText } = render(<ValidEmail email={EMAIL_USER} />);
+  const isValid = getByText('Email Valido');
+  expect(isValid).toBeInTheDocument();
+});
+
+test('Testando um componente, caso o email seja inválido.', () => {
+  const EMAIL_USER = 'emailerrado';
+  const { getByText } = render(<ValidEmail email={EMAIL_USER} />);
+  const isValid = getByText('Email Inválido');
+  expect(isValid).toBeInTheDocument();
+});
+
+test('Testando um componente, caso o email ainda não tenha sido passado.', () => {
+  const EMAIL_USER = '';
+  const { queryByAltText } = render(<ValidEmail email={EMAIL_USER} />);
+  expect(queryByAltText('Email Inválido')).not.toBeInTheDocument()
+  expect(queryByAltText('Email Validos')).not.toBeInTheDocument()
+});

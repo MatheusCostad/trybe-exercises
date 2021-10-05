3aa65c6baff3923086912046d9dd5d51bfa1d618
Matheus Costa <mathws.costag@gmail.com>
2021-10-05 11:27:18 -0300

    fibonacci.js

--- 3 - Desenvimento Back-end/Bloco 26 - Introdução ao desenvolvimento Web com NodeJS/1 - Node.js - Introdução/Exercise/fibonacci.js
+++ 3 - Desenvimento Back-end/Bloco 26 - Introdução ao desenvolvimento Web com NodeJS/1 - Node.js - Introdução/Exercise/fibonacci.js
@@ -3,7 +3,7 @@ const readline = require('readline-sync');
 function fibonacciSequency(number) {
   let fibonacciSequency = [1, 1];
   if (number <= 0) return 'Número inválido';
-  if (number === 1) return '1';
+  if (number === 1) return [1];
   for(index = 2; index <  number; index += 1) {
     fibonacciSequency = [ ...fibonacciSequency, (fibonacciSequency[index - 1] + fibonacciSequency[index - 2])];
   }

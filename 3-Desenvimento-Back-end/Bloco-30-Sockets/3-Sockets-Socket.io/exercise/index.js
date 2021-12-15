1553f4d2cdfd0fd8ffa6da5a254737bdad9af628
Matheus Costa <mathws.costag@gmail.com>
2021-12-15 15:06:17 -0300

    Exercício

--- 3-Desenvimento-Back-end/Bloco-30-Sockets/3-Sockets-Socket.io/exercise/index.js
+++ 3-Desenvimento-Back-end/Bloco-30-Sockets/3-Sockets-Socket.io/exercise/index.js
@@ -4,15 +4,13 @@ const http = require('http').createServer(app);
 
 const io = require('socket.io')(http, {
 cors: {
-  origin: 'http://localhost:3000', // url aceita pelo cors
-  methods: ['GET', 'POST'], // Métodos aceitos pela url
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST'],
 }});
 
 app.use(express.static(__dirname + '/public'));
 
-require('./sockets/ping')(io);
-require('./sockets/chat')(io);
-require('./sockets/rooms')(io);
+require('./sockets/feed')(io);
 
 http.listen(3000, () => {
   console.log('Servidor ouvindo na porta 3000');

9ce4aa832a2e4949ad753a33002b268d5003de50
dcmatheus <mathws.costag@gmail.com>
2022-01-20 01:15:46 -0300

    Example list

--- 4-Ciencia-da-Computacao/Bloco-32-Introducao-a-Python/1-Aprendendo-Python/Exemplos/list.py
+++ 4-Ciencia-da-Computacao/Bloco-32-Introducao-a-Python/1-Aprendendo-Python/Exemplos/list.py
@@ -27,3 +27,23 @@ print(info)
 print("Exercício 10")
 del info["origem"]
 print(info)
+
+print("Exercício 13")
+factorial = 1
+print("Initial value", 5)
+for index in range(1, 6):
+    factorial *= index
+print("Factorial", factorial)
+
+
+print("Exercício 14/15")
+ratings = [6, 8, 5, 9, 10]
+print("Old ratings", ratings)
+
+for index, rating in enumerate(ratings):
+    ratings[index] = rating * 10
+    if (rating % 3) == 0:
+        print(rating, "Múltiplo de 3")
+
+
+print("new ratings", ratings)

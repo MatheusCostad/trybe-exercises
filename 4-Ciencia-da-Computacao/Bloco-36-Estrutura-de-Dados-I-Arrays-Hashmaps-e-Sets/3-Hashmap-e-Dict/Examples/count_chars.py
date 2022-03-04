73614c4a9ea9738ab9b6622a85e227d32ac11e20
dcmatheus <mathws.costag@gmail.com>
2022-03-04 00:08:48 -0300

    count_chars example

---
+++ 4-Ciencia-da-Computacao/Bloco-36-Estrutura-de-Dados-I-Arrays-Hashmaps-e-Sets/3-Hashmap-e-Dict/Examples/count_chars.py
@@ -0,0 +1,10 @@
+str = "anaconda"
+count_chars = {}
+
+for char in str:
+    if char not in count_chars:
+        count_chars[char] = 1
+    else:
+        count_chars[char] += 1
+
+print(count_chars)

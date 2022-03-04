5a52940f727f8032cbbdead1bf109149ef57ddfa
dcmatheus <mathws.costag@gmail.com>
2022-03-03 23:39:34 -0300

    dict example

--- 4-Ciencia-da-Computacao/Bloco-36-Estrutura-de-Dados-I-Arrays-Hashmaps-e-Sets/3-Hashmap-e-Dict/Examples/hashmap.py
+++ 4-Ciencia-da-Computacao/Bloco-36-Estrutura-de-Dados-I-Arrays-Hashmaps-e-Sets/3-Hashmap-e-Dict/Examples/hashmap.py
@@ -45,3 +45,22 @@ print(hashable_employees.get_value(23))
 print(hashable_employees.get_value(10))
 hashable_employees.update_value(10, "Rafaela")
 print(hashable_employees.get_value(10))
+
+
+class Separate_chaining_hash_map:
+    def __init__(self):
+        self._buckets = [[] for i in range(10)]
+
+    def get_address(self, id_num):
+        return id_num % 10
+
+    def insert(self, employee):
+        address = self.get_address(employee.id_num)
+        self._buckets[address].append(employee)
+
+    def get_value(self, id_num):
+        address = self.get_address(id_num)
+        for item in self._buckets[address]:
+            if item.id_num == id_num:
+                return item.name
+        return None

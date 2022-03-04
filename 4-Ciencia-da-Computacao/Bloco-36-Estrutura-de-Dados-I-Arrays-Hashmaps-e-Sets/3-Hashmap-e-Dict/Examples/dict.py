d49f2e32eeff6cec25d190253573b52dcca741e6
dcmatheus <mathws.costag@gmail.com>
2022-03-04 00:10:44 -0300

    triple example

--- 4-Ciencia-da-Computacao/Bloco-36-Estrutura-de-Dados-I-Arrays-Hashmaps-e-Sets/3-Hashmap-e-Dict/Examples/dict.py
+++ 4-Ciencia-da-Computacao/Bloco-36-Estrutura-de-Dados-I-Arrays-Hashmaps-e-Sets/3-Hashmap-e-Dict/Examples/dict.py
@@ -20,6 +20,12 @@ double = {i: i * 2 for i in range(3, 21)}
 
 print(double)
 
+for key in double.keys():
+    if key % 2 != 0:
+        double[key] = key * 3
+
+print(double)
+
 
 group = ChainMap(
     {"nome": "Cássio", "idade": "30"},

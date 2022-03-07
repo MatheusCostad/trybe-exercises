3fef01aedb0282ed9c8a2980532b8dc4583afe9e
dcmatheus <mathws.costag@gmail.com>
2022-03-07 18:07:07 -0300

    add intersection in class Conjunto

--- 4-Ciencia-da-Computacao/Bloco-36-Estrutura-de-Dados-I-Arrays-Hashmaps-e-Sets/4-Set/Examples/conjunto.py
+++ 4-Ciencia-da-Computacao/Bloco-36-Estrutura-de-Dados-I-Arrays-Hashmaps-e-Sets/4-Set/Examples/conjunto.py
@@ -31,6 +31,15 @@ class Conjunto:
 
         return new_conjunto
 
+    def intersection(self, conjuntoB):
+        new_conjunto = Conjunto()
+
+        for index in range(1001):
+            if self.set[index] and conjuntoB.set[index]:
+                new_conjunto.add(index)
+
+        return new_conjunto
+
 
 if __name__ == "__main__":
     conj = Conjunto()
@@ -52,3 +61,25 @@ if __name__ == "__main__":
 
     conj4 = Conjunto()
     print(conj4)
+
+    conj1 = Conjunto()
+    for i in range(1, 11):
+        conj1.add(i)
+
+    conj2 = Conjunto()
+    for i in range(10, 21):
+        conj2.add(i)
+
+    conj3 = conj1.union(conj2)
+    print(conj3)
+
+    conj1 = Conjunto()
+    for i in [1, 2, 3]:
+        conj1.add(i)
+
+    conj2 = Conjunto()
+    for i in [7, 2, 10]:
+        conj2.add(i)
+
+    conj3 = conj1.intersection(conj2)
+    print(conj3)

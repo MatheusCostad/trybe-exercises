0ccbcfd3863a4f1803a778ebea6b26eede1c8e14
dcmatheus <mathws.costag@gmail.com>
2022-03-07 17:49:03 -0300

    add __str__ in class Conjunto

--- 4-Ciencia-da-Computacao/Bloco-36-Estrutura-de-Dados-I-Arrays-Hashmaps-e-Sets/4-Set/Examples/conjunto.py
+++ 4-Ciencia-da-Computacao/Bloco-36-Estrutura-de-Dados-I-Arrays-Hashmaps-e-Sets/4-Set/Examples/conjunto.py
@@ -9,9 +9,32 @@ class Conjunto:
         if item > self.last_element:
             self.last_element = item
 
+    def __str__(self):
+        string = "{"
+        for index, is_in_set in enumerate(self.set):
+            if is_in_set:
+                string += str(index)
+                if index < self.last_element:
+                    string += ", "
+        string += "}"
+        return string
+
 
 if __name__ == "__main__":
     conj = Conjunto()
+    for i in [0, 10, 100, 1000]:
+        conj.add(i)
+    print(conj)
 
-    for item in [0, 10, 100, 1000]:
-        conj.add(item)
+    conj2 = Conjunto()
+    for i in [1, 2, 3]:
+        conj2.add(i)
+    print(conj2)
+
+    conj3 = Conjunto()
+    for i in [7, 2, 10]:
+        conj3.add(i)
+    print(conj3)
+
+    conj4 = Conjunto()
+    print(conj4)
